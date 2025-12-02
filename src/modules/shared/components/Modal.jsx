d601b55171d3a440241a0dd95dd018f2ec35f86e
josematias5 <jomatias108@gmail.com>
2025-12-02 10:37:08 -0300

    Confetis wuuujuuuuugit add .git add .!

--- src/modules/shared/components/Modal.jsx
+++ src/modules/shared/components/Modal.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Card from "./Card";
 
-function Modal({ isOpen, onClose, children }) {
+function Modal({ isOpen, onClose, children, className = "" }) {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
+        <div className={`fixed inset-0 flex items-center justify-center bg-black/50 z-50 ${className}`}>
             <Card className="relative w-full max-w-lg mx-4 p-10">
+                {children}
                 <button
                     onClick={onClose}
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
                 >
                 ✕
                 </button>
-                {children}
+                
             </Card>
         </div>
     );

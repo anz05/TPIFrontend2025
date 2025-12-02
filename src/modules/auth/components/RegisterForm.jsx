160ec861d46744b6c47b7a911c771531b3592fa6
josematias5 <jomatias108@gmail.com>
2025-12-01 21:46:30 -0300

    Alerta de registro

--- src/modules/auth/components/RegisterForm.jsx
+++ src/modules/auth/components/RegisterForm.jsx
@@ -6,8 +6,9 @@ import Button from '../../shared/components/Button';
 import { register as registerService } from '../services/register';
 import { frontendErrorMessage } from '../helpers/backendError';
 import Card from '../../shared/components/Card';
+import Modal from '../../shared/components/Modal';
 
-function RegisterForm({hasRole}) {
+function RegisterForm({ hasRole }) {
     const { register, handleSubmit, watch, formState: { errors } } = useForm({
         defaultValues: { username: '', email: '', role: 'USER', password: '', confirmPassword: '' }
     });
@@ -15,110 +16,136 @@ function RegisterForm({hasRole}) {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
+    const [modalOpen, setModalOpen] = useState(false);
+    const [createdUserMsg, setCreatedUserMsg] = useState('');
+
     const onSubmit = async (values) => {
-    if (values.password !== values.confirmPassword) {
-        setServerError('Las contraseñas no coinciden');
-        return;
-    }
-    const finalRole = hasRole ? values.role : 'USER';
-    try {
-        setLoading(true);
-        const { data, error } = await registerService({
-            username: values.username,
-            email: values.email,
-            role: finalRole,
-            password: values.password,
-        });
-        if (error) {
-            setServerError(error);
+        if (values.password !== values.confirmPassword) {
+            setServerError('Las contraseñas no coinciden');
             return;
         }
-        alert(data || 'Usuario creado');
-        navigate('/'); 
-    } catch (err) {
-        console.error(err);
+        const finalRole = hasRole ? values.role : 'USER';
+        try {
+            setLoading(true);
+            const { data, error } = await registerService({
+                username: values.username,
+                email: values.email,
+                role: finalRole,
+                password: values.password,
+            });
+            if (error) {
+                setServerError(error);
+                return;
+            }
+            setCreatedUserMsg(data || 'Usuario creado correctamente');
+            setModalOpen(true);
+        } catch (err) {
+            console.error(err);
 
-        const code = err?.response?.data?.code;
+            const code = err?.response?.data?.code;
 
-        if (code) {
-            setServerError(frontendErrorMessage[code] || err?.response?.data?.message || 'Error al crear usuario');
-        } else {
-            setServerError(err?.response?.data || 'Error al crear usuario');
+            if (code) {
+                setServerError(frontendErrorMessage[code] || err?.response?.data?.message || 'Error al crear usuario');
+            } else {
+                setServerError(err?.response?.data || 'Error al crear usuario');
+            }
+
+            console.error(err);
+        } finally {
+            setLoading(false);
         }
-
-        console.error(err);
-    } finally {
-        setLoading(false);
-    }
     };
 
     return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-        <Card>
-            <Input 
-                label="Usuario" 
-                {...register(
-                    'username', 
-                    { required: 'Usuario es obligatorio' }
-                )} 
-                error={errors.username?.message} 
-            />
-            <Input 
-                label="Email" 
-                {...register(
-                    'email', 
-                    { 
-                        required: 'Email es obligatorio', 
-                        pattern: { value: /^\S+@\S+$/i, message: 'Email inválido' } 
-                    }
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <Card>
+                <Input
+                    label="Usuario"
+                    {...register(
+                        'username',
+                        { required: 'Usuario es obligatorio' }
+                    )}
+                    error={errors.username?.message}
+                />
+                <Input
+                    label="Email"
+                    {...register(
+                        'email',
+                        {
+                            required: 'Email es obligatorio',
+                            pattern: { value: /^\S+@\S+$/i, message: 'Email inválido' }
+                        }
                     )
-                } 
-                error={errors.email?.message} 
-            />
-            {hasRole && (
-                <label className="block">
-                    Rol
-                    <select 
-                        {...register('role')} 
-                        className="block w-full mt-1 p-2 border border-gray-300 rounded-md"
-                    >
-                        <option value="USER">User</option>
-                        <option value="ADMIN">Admin</option>
-                    </select>
-                </label>
-            )}
-            
-            <Input 
-                label="Contraseña" 
-                type="password" 
-                {...register(
-                    'password', 
-                    { 
-                        required: 'Contraseña obligatoria', 
-                        minLength: { value: 6, message: 'Mínimo 6 caracteres' } 
                     }
+                    error={errors.email?.message}
+                />
+                {hasRole && (
+                    <label className="block">
+                        Rol
+                        <select
+                            {...register('role')}
+                            className="block w-full mt-1 p-2 border border-gray-300 rounded-md"
+                        >
+                            <option value="USER">User</option>
+                            <option value="ADMIN">Admin</option>
+                        </select>
+                    </label>
+                )}
+                <Input
+                    label="Contraseña"
+                    type="password"
+                    {...register(
+                        'password',
+                        {
+                            required: 'Contraseña obligatoria',
+                            minLength: { value: 6, message: 'Mínimo 6 caracteres' }
+                        }
                     )
-                } error={errors.password?.message} 
-            />
-            <Input 
-                label="Confirmar Contraseña" 
-                type="password" 
-                {...register(
-                    'confirmPassword', 
-                    { 
-                        required: 'Confirmación obligatoria' 
-                    })
-                } 
-                error={errors.confirmPassword?.message} 
-            />
-            <Button 
-                type="submit" 
-                disabled={loading}>
+                    } error={errors.password?.message}
+                />
+                <Input
+                    label="Confirmar Contraseña"
+                    type="password"
+                    {...register(
+                        'confirmPassword',
+                        {
+                            required: 'Confirmación obligatoria'
+                        })
+                    }
+                    error={errors.confirmPassword?.message}
+                />
+                <Button
+                    type="submit"
+                    disabled={loading}>
                     {loading ? 'Creando...' : 'Crear usuario'}
-            </Button>
-            {serverError && <p className="text-red-500">{serverError}</p>}
-        </Card>
-    </form>
+                </Button>
+                {serverError && <p className="text-red-500">{serverError}</p>}
+                <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+                    <div className="flex flex-col items-center gap-4">
+                        <h2 className="text-xl font-semibold text-center">
+                            {createdUserMsg}
+                        </h2>
+                        <p className="text-gray-600 text-center">
+                            Tu cuenta ha sido creada exitosamente.
+                        </p>
+                        <div className="flex flex-col w-full gap-3 mt-2">
+                            <button
+                                className="bg-purple-200 hover:bg-purple-300 text-black text-xm px-3 py-3 rounded-xl"
+                                onClick={() => navigate('/login')}
+                            >
+                                Iniciar sesión
+                            </button>
+                            <button
+                                className="w-full border border-gray-300 py-2 rounded-xl hover:bg-gray-100"
+                                onClick={() => navigate('/')}
+                            >
+                                Salir
+                            </button>
+                        </div>
+                    </div>
+                </Modal>
+            </Card>
+        </form>
     );
 }
 
